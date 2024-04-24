c2fd8cd389c8a5e0af8e7ccaad431623cef176b9
G1ul1o <103744749+G1ul1o@users.noreply.github.com>
2024-04-24 18:40:20 +0200

    Affichage fini

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from lecture_fichier import *
 from fonction_général import *
+from Nord_Ouest import *
 
 
 if __name__ == '__main__':
@@ -25,6 +26,15 @@ if __name__ == '__main__':
         affichage_proposition_de_transport(matrice,matrice,nbr_C,nbr_P)
 
 
+    def Methode_NO():
+        global matrice_NO
+        matrice_NO = Nord_Ouest(matrice, nbr_C, nbr_P)
+
+        print("La matrice avec la méthode de Nord-Ouest est :")
+        for row in matrice_NO:
+            print(row)
+
+
 
 
 
@@ -34,6 +44,7 @@ if __name__ == '__main__':
         if choix == 1:
             lire_fichier()
             afficher_matrices()
+            Methode_NO()
 
 
         if continuer == True:
