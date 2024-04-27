d3190a3f388143e2eb0ff3a5a9e73b546b7b8111
G1ul1o <103744749+G1ul1o@users.noreply.github.com>
2024-04-27 16:43:16 +0200

    Cycle renvoyé

--- Verif_dégénérée.py
+++ Verif_dégénérée.py
@@ -40,10 +40,12 @@ def verif_cycle (graph):
 
     sommet_parcouru = []
 
+
     #On récupère C1
     index = recherche_indice(graph,"C1")
     C1 = graph[index]
     sommet_parcouru.append("C1")
+
 
     #On passe dans les successeurs de C1
     for liaison in C1.liaison:
@@ -51,13 +53,30 @@ def verif_cycle (graph):
 
         index = recherche_indice(graph, liaison)
 
-        verif,sommet_parcouru = verif_cycle_recursif(graph[index],"C1",sommet_parcouru,graph,True)
+        verif,sommet_cycle= verif_cycle_recursif(graph[index],"C1",sommet_parcouru,graph,True)
 
-    return verif
+    if verif == False:
+        cycle = []
 
+        index = recherche_indice(graph,sommet_cycle)
+
+        for liaison in (graph[index].liaison):
+            chemin = []
+            chemin.append(sommet_cycle)
+            #On cherche la première liaison
+            index = recherche_indice(graph,liaison)
+
+            chemin.append(liaison)
+            recherche_cycle(graph[index],sommet_cycle,cycle,graph,sommet_cycle,chemin)
+
+            if len(cycle) > 0:
+                return verif,cycle
+
+    return  verif,cycle
 
 def verif_cycle_recursif(sommet,sommet_parent,sommet_parcouru,graph,verif):
 
+    sommet_cycle="A"
     for liaison in sommet.liaison:
 
         if verif == True:
@@ -66,19 +85,44 @@ def verif_cycle_recursif(sommet,sommet_parent,sommet_parcouru,graph,verif):
 
                 index = recherche_indice(graph,liaison)
                 sommet_parcouru.append(liaison)
-                verif,sommet_parcouru = verif_cycle_recursif(graph[index],sommet.nom_sommet,sommet_parcouru,graph,verif)
+
+                verif,sommet_cycle = verif_cycle_recursif(graph[index],sommet.nom_sommet,sommet_parcouru,graph,verif)
 
             elif liaison in sommet_parcouru and liaison != sommet_parent:
-                        return False,sommet_parcouru
+                sommet_cycle = liaison
+                return False,sommet_cycle
         else:
-            return False,sommet_parcouru
+            return False,sommet_cycle
 
     #Nous sommes arrivés à un sommet qui n'a pas d'autres successeur que son parent
-    return verif, sommet_parcouru
+    return verif,sommet_cycle
+
+def recherche_cycle (sommet,sommet_parent,cycle,graph,sommet_départ,chemin_parcouru):
+
+    if sommet.nom_sommet == sommet_départ:
+        for i in range (len(chemin_parcouru)):
+            cycle.append(chemin_parcouru[i])
+        return
+
+    # On récupère les liaisons dans le graph
+    for liaison in sommet.liaison:
+
+        if liaison != sommet_parent:
+            stockage_chemin_intermediaire = []
+
+            for i in range(len(chemin_parcouru)):
+                stockage_chemin_intermediaire.append(chemin_parcouru[i])
 
 
+            stockage_chemin_intermediaire.append(liaison)
+            index = recherche_indice(graph,liaison)
+            recherche_cycle(graph[index], sommet.nom_sommet, cycle,graph,sommet_départ,stockage_chemin_intermediaire)
+    return
+def Maximisation (graph,cycle):
+    print()
 
-'''proposition_de_transport1 = [[5,0,30,25],[0,30,0,0],[45,45,0,0]]
+
+proposition_de_transport1 = [[5,5,0,0],[0,30,2,3],[0,0,10,10]]
 proposition_de_transport2 = [[0,2000,3000,0],[0,1000,2000,3000],[5000,0,0,1000]]
 nbr_C = 4
 nbr_S = 3
@@ -94,7 +138,7 @@ for i in range(len(graph1)):
 verif = verif_cycle(graph1)
 
 print(verif)
-
+'''
 for i in range(len(graph2)):
     print(graph2[i].nom_sommet ,",", graph2[i].liaison)
     if graph2[i].nom_sommet=="C1":
